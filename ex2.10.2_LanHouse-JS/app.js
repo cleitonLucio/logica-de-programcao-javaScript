79c48dbbbee2146018334b99f312bf92a0114de6
cleiton.Lucio <cleitonl.carmo@gmail.com>
2023-01-31 21:05:45 -0300

    criando pagina javaScript

--- ex2.10.2_LanHouse-JS/app.js
+++ ex2.10.2_LanHouse-JS/app.js
@@ -1,1 +1,15 @@
 // Math.ceil(num)
+function calcularValorPTempo(){
+    var inPreco = document.getElementById("inPreco");
+    var inTempo = document.getElementById("inTempo");
+    var outResposta = document.getElementById("outResposta");
+
+    var valorAPagar = Number(inPreco.value);
+    var tempoCliente = Math.ceil(inTempo.value/15); 
+    var resultoAPagar = Number(valorAPagar*tempoCliente);
+
+    outResposta.textContent = "Valor a Pagar R$: " + resultoAPagar
+}
+
+var btCalcular = document.getElementById("btCalcular");
+btCalcular.addEventListener("click", calcularValorPTempo);
